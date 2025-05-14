d523b863542df1976101f58668d75a0e3f9bf233
Filips457 <artemfilipson@gmail.com>
2025-05-14 22:22:55 +0300

    Visual added
    Black background
    Partial system (stars)
    Hp bar

--- 2d laba/Assets/Code/Player/PlayerHealth.cs
+++ 2d laba/Assets/Code/Player/PlayerHealth.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int StartHealth;
+    [SerializeField] private Image HealthBarImage;
 
     private int health;
 
@@ -18,6 +20,8 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        HealthBarImage.fillAmount = (float)health / StartHealth;
 
         if (health <= 0)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
